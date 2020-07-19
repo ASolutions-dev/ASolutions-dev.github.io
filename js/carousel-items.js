aed598cdd37cd70cb45793e6761d019f60352f9d
Adrian <a.snoch@gmail.com>
2020-07-19 12:27:49 +0200

    Add vue.js framework

---
+++ js/carousel-items.js
@@ -0,0 +1,26 @@
+var carouselItems = [
+    {
+        title: "Schody i balustrady",
+        description: "Profesjonalne wykonanie według indywidualnych zamówień",
+        class: "item active d-block w-100",
+        style: "background-image: url(images/slider-bg-1-2.jpg)"
+    },
+    {
+        title: "Schody samonośne",
+        description: "Wytrzymałe i eleganckie konstrukcje",
+        class: "item d-block w-100",
+        style: "background-image: url(images/slider-bg-2-2.jpg)"
+    },
+    {
+        title: "Schody na beton",
+        description: "Klasyczne rozwiązania na lata",
+        class: "item d-block w-100",
+        style: "background-image: url(images/slider-bg-3-2.jpg)"
+},
+    {
+        title: "Balustrady",
+        description: "Mnogość materiałów i projektów",
+        class: "item d-block w-100",
+        style: "background-image: url(images/slider-bg-4-2.jpg)"
+    }
+]
