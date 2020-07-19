856651332558417fad8a4c853cf9e7ef40cef652
Adrian <a.snoch@gmail.com>
2020-07-19 12:53:14 +0200

    Move portoflio gallery to vue.js

---
+++ js/portfolio.js
@@ -0,0 +1,42 @@
+var portfolio = [
+    {
+        class: "portfolio-item responsive samonosne",
+        thumbnail: "images/portfolio/thumb/samonosne1.jpg",
+        image: "images/portfolio/full/samonosne1.jpg"
+    },
+    {
+        class: "portfolio-item responsive samonosne",
+        thumbnail: "images/portfolio/thumb/samonosne2.jpg",
+        image: "images/portfolio/full/samonosne2.jpg"
+    },
+    {
+        class: "portfolio-item responsive beton",
+        thumbnail: "images/portfolio/thumb/beton1.jpg",
+        image: "images/portfolio/full/beton1.jpg"
+    },
+    {
+        class: "portfolio-item responsive beton",
+        thumbnail: "images/portfolio/thumb/beton2.jpg",
+        image: "images/portfolio/full/beton2.jpg"
+    },
+    {
+        class: "portfolio-item responsive beton",
+        thumbnail: "images/portfolio/thumb/beton3.jpg",
+        image: "images/portfolio/full/beton3.jpg"
+    },
+    {
+        class: "portfolio-item responsive balustrady",
+        thumbnail: "images/portfolio/thumb/balustrady1.jpg",
+        image: "images/portfolio/full/balustrady1.jpg"
+    },
+    {
+        class: "portfolio-item responsive balustrady",
+        thumbnail: "images/portfolio/thumb/balustrady2.jpg",
+        image: "images/portfolio/full/balustrady2.jpg"
+    },
+    {
+        class: "portfolio-item responsive balustrady",
+        thumbnail: "images/portfolio/thumb/balustrady3.jpg",
+        image: "images/portfolio/full/balustrady3.jpg"
+    }
+]
